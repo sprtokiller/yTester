344a676d04135ff059ce071fa1238cbd29a6da35
Vítězslav Kříž <sprtokiller.6c@gmail.com>
2023-04-27 14:43:00 +0200

    chore: Add authentication functionality and dependencies.
    - Add new dependencies to package.json
    - Export `verifyCredentials` function in src/app/auth.ts
    - Add SESSION_KEY environment variable to .env file.

--- src/app/auth.ts
+++ src/app/auth.ts
@@ -2,12 +2,10 @@ import { OAuth2Client } from 'google-auth-library'
 const client = new OAuth2Client(process.env.GOOGLE_CLIENT_ID)
 
 // Call this function to validate the JWT credential sent from client-side
-const verifyCredentials = async (credential : string) => {
+export async function verifyCredentials(credential : string) {
   const ticket = await client.verifyIdToken({
     idToken: credential
   })
   const payload = ticket.getPayload()
   return payload
 }
-
-export default verifyCredentials
